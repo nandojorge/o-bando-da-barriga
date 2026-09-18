60dcb001f841db6fc4bf0dafbb7bfc4d47d4d409
nandojorge <52052668+nandojorge@users.noreply.github.com>
2026-09-18 23:45:27 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Cola aqui os dois valores do teu projeto Supabase.
+// A Publishable key é segura para utilização numa app pública no browser.
+window.APP_CONFIG = {
+  SUPABASE_URL: https://laudtajjkjmugybsrmny.supabase.co,
+  SUPABASE_PUBLISHABLE_KEY: sb_publishable_NxMDXbswnBRpmFoh0vILyw_yqBTJQH4
+};
